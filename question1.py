d483e4309ad5a85bc36ce0ba24b17ee98b2967ab
Ashwin Menon <117213148+X3PH1RE@users.noreply.github.com>
2023-10-21 21:46:51 +0530

    Update question1.py
    This version of the code has the feature where it is asked to the user  if they want to exit or not. Comments have also been added.

--- question1.py
+++ question1.py
@@ -1,13 +1,25 @@
-count=0
-tot=0
-repeat=[]
+count=0    #sets the denominator value to zero, so the number of elements is updated after every iteration 
+tot=0      #sets the value of the sum of the numbers whose average is to be found to zero
+repeat=[]  #an empty set which stores the numbers inputed to check it is repeated later or not
 while 1:
-    a=float(input("Enter a number: "))
-    if a in repeat:
+    a=float(input("Enter a number: "))  
+
+    if a in repeat:       #this loop checks if the number is being repeated or not 
         print(avg)
-        continue
-    repeat.append(a)
+        d=input(("Do you want to exit?(y/n) "))
+        if(d=='y'):
+            break
+        else:
+            continue
+    repeat.append(a)      #adds the newly inputed value to the set 
     count+=1
     tot+=a
-    avg=tot/count
+    avg=tot/count         #calculates the average of the previously inputed numbers
     print(avg)
+
+    d=input(("Do you want to exit?(y/n) "))
+    if(d=="y"):
+        break
+    
+    
+    
